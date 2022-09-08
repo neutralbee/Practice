baa0c4ce596af8a8f5044cebcad4572ea7d95829
Jeff T <jtung1217@gmail.com>
2022-09-07 20:14:17 -0700

    Add getWinner function

--- the_odin_project/odin-rock-paper-scssors/javascript.js
+++ the_odin_project/odin-rock-paper-scssors/javascript.js
@@ -1,8 +1,8 @@
 // play one round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
 //get player choice
-let playerOption = getPlayerChoice;
-return (playerOption);
+//let playerOption = getPlayerChoice;
+//return (playerOption);
 //get computer choice
 //determine winner
 }
@@ -15,7 +15,8 @@ function getRndInteger(min, max) {
 // uses random integer to generate computer's choice
 function getComputerChoice() {
 const rndInteger = getRndInteger(1,3);
-
+  
+  //can refactor and use switch() instead
   if (rndInteger == 1) {
     return "rock";
   } else if (rndInteger == 2) {
@@ -41,4 +42,34 @@ function getPlayerChoice() {
       playerChoice = playerChoice.toLowerCase();
     }
     }
+}
+
+function getWinner(playerPick, computerPick) {
+  switch (playerPick) {
+    case computerPick:
+      return "tie";
+      break;
+    case "rock":
+      if (computerPick == "scissors") {
+      return "win";
+      } else{
+        return "lose"
+      }
+      break;
+    case "paper":
+      if (computerPick == "rock") {
+        return "win";
+        } else{
+          return "lose"
+        }
+        break;
+    case "scissors":
+      if (computerPick == "paper") {
+        return "win";
+        } else{
+          return "lose"
+        }
+        break;
+  }
+  
 }
